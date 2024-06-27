3406c4180616a89efcab33c3286f1ff2fc7a8fa9
harub0 <h13.0404@icloud.com>
2024-06-27 12:22:28 +0900

    データ取得件数の制限

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,6 +9,10 @@ class PostController extends Controller
 {
     public function index(Post $post)
     {
-        return view('posts.index')->with(['posts' => $post->get()]);
+        // check SQL code
+        // $test = $post->orderBy('updated_at', 'DESC')->limit(2)->toSql();
+        // dd($test);
+        
+        return view('posts.index')->with(['posts' => $post->getByLimit()]);
     }
 }
