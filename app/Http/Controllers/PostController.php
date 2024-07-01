14124b6b51502df5449d957002f448f43589af3d
harub0 <h13.0404@icloud.com>
2024-07-01 19:25:55 +0900

    teratail APIで質問一覧取得

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,11 +10,35 @@ class PostController extends Controller
 {
     public function index(Post $post)
     {
+        // クライアントインスタンス生成
+        $client = new \GuzzleHttp\Client();
+
+        // GET通信するURL
+        $url = 'https://teratail.com/api/v1/questions';
+
+        // リクエスト送信と返却データの取得
+        // Bearerトークンにアクセストークンを指定して認証を行う
+        $response = $client->request(
+            'GET',
+            $url,
+            ['Bearer' => config('services.teratail.token')]
+        );
+        
+        // API通信で取得したデータはjson形式なので
+        // PHPファイルに対応した連想配列にデコードする
+        $questions = json_decode($response->getBody(), true);
+        
+        // index bladeに取得したデータを渡す
+        return view('posts.index')->with([
+            'posts' => $post->getPaginateByLimit(),
+            'questions' => $questions['questions'],
+        ]);
+        
         // SQLの確認用
         // $test = $post->orderBy('updated_at', 'DESC')->limit(2)->toSql();
         // dd($test);
         
-        return view('posts.index')->with(['posts' => $post->getPaginateByLimit()]);
+        // return view('posts.index')->with(['posts' => $post->getPaginateByLimit()]);
     }
     
     public function show(Post $post)
